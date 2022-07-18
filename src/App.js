c9a29da8c4281d2b40e370422a8f20ce6f1d1730
PamelaCleveland <pamelac021@gmail.com>
2022-07-18 16:09:21 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from "./components/Gallery";
+import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
         currentCategory={currentCategory}
         contactSelected={contactSelected}
         setContactSelected={setContactSelected}
-      />
+      ></Nav>
       <main>
         {!contactSelected ? (
           <>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
