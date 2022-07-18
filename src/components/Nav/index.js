62fa14af06b77f8514b3ce48e63889f4013eded7
PamelaCleveland <pamelac021@gmail.com>
2022-07-18 17:38:13 -0400

    bug fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,7 +17,7 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h2>
-        <a data-testid="link" href="/">
+        <a data-testid="link" href="http://pamelac21.github.io/Oh-Snap">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
